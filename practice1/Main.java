4df1fa71caabe0f3cdadd049b0b74cffdbcafd21
droid-101 <49104581+droid-101@users.noreply.github.com>
2021-10-22 23:21:33 -0400

    Practice1 Exercise
    New exercise to overview loops and conditional statements in loops. Includes an exercise.

---
+++ practice1/Main.java
@@ -0,0 +1,10 @@
+/* 
+ * NOTE: In order to compile and run your code, you must make sure that your terminal path 
+ * is the path of the "practice1" folder. You can use the command "cd" to move into a folder.
+ * "cd .." can move back out of a folder. Once you are "in" the practice1 folder, the javac
+ * and java commands will work.
+ */ 
+
+ // Also note that with loops, your program can sometimes get stuck. To stop a stuck program, use "control c".
+
+ // Write your code here!
