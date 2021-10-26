9ab6e26a4f6f08556a68307bceb1bf462e5881fb
LegenWaitForIt-Dary <Wallsallw@gmail.com>
2021-10-25 22:35:57 -0400

    Methods lesson
    Combines everything we've learned so far with methods.

--- practice0/Main.java
+++ practice0/Main.java
@@ -1,1 +1,28 @@
-// Write your code here!
+import java.util.*; //
+
+class Main 
+{
+    public static void main( String [] args){
+        Scanner uI = new Scanner(System.in);    //
+      int l=0;
+      int w=0;
+        do{                                     //
+        System.out.println("Please enter the length of your rectangle in cm");
+        l = uI.nextInt();
+        System.out.println("Please enter the width of your rectangle in cm");
+        w = uI.nextInt();
+        if(!(l>=1 && l<=5 && w>=1 && w<=5))
+        check();
+        }while(!(l>=1 && l<=5 && w>=1 && w<=5));         // && ||
+        rectangle(l,w);                        //name(*param*s)
+        }
+    public static void rectangle(int l, int w){      //public static void Nameof|Method(*optional*=>parameters){}
+        Scanner uI = new Scanner(System.in);
+        System.out.println("The area of your rectangle is " +l*w+" cm");
+    } 
+    public static void check(){
+        System.out.println("This is a mistake");
+        System.out.println("Please enter the right numbers");
+        System.out.println("The number should be between 1 and 5");
+    }
+}
